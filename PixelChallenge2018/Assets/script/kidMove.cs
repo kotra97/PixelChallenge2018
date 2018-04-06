8aa12a744484fec8f3da7d408048ce81a999f086
Kotra <mathisr2207@hotmail.fr>
2018-04-06 17:15:15 +0200

    add sprite and test scene

---
+++ PixelChallenge2018/Assets/script/kidMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kidMove : MonoBehaviour {
+
+    Vector3 moveDirection;
+    public GameObject door;
+    float speed = 0.09f;
+    float secondSpeed = 0.3f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = new Vector3(Input.GetAxis("Joystick Axe X"), 0, 0);
+        moveDirection = transform.TransformDirection(moveDirection);
+        if (Input.GetButton("Joystick A"))
+            moveDirection *= secondSpeed;
+        else
+            moveDirection *= speed;
+        transform.position += moveDirection;
+    }
+}
