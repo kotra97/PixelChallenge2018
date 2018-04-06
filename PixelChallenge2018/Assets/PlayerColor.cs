428e0e107a817d0092ccccf32f33d72e740ffde5
Nico Lear <learjet45@hotmail.fr>
2018-04-06 16:07:57 -0400

    Player add color

---
+++ PixelChallenge2018/Assets/PlayerColor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColor : MonoBehaviour {
+
+	private SpriteRenderer sr;
+	private int isColored = 0;
+	// Use this for initialization
+	void Start () {
+		sr = gameObject.GetComponent<SpriteRenderer>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Color") 
+		{
+			float red = other.gameObject.GetComponent<SpriteRenderer>().color.r;
+			float gre = other.gameObject.GetComponent<SpriteRenderer>().color.g;
+			float blu = other.gameObject.GetComponent<SpriteRenderer>().color.b;
+			if (isColored == 1)
+			{
+				red += sr.color.r;
+				gre += sr.color.g;
+				blu += sr.color.b;
+			}
+			else
+			{
+				isColored = 1;
+			}
+			sr.color = new Color(red, gre, blu);
+			Destroy(other.gameObject);
+		}
+	}
+}
