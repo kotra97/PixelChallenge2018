22ac3b7d91b3058f15602f4704d24dbdd5d6e236
Kotra <mathisr2207@hotmail.fr>
2018-04-06 23:16:43 +0200

    ChangeColor End

---
+++ PixelChallenge2018/Assets/script/PlayerColor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColor : MonoBehaviour {
+
+	private SpriteRenderer sr;
+	// Use this for initialization
+	void Start ()
+    {
+		sr = gameObject.GetComponent<SpriteRenderer>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+    Color additionColor(Color one, Color two)
+    {
+        float red = one.r;
+        float gre = one.g;
+        float blu = one.b;
+        red += two.r;
+        gre += two.g;
+        blu += two.b;
+
+        if (red > 255)
+            red = 255;
+        if (blu > 255)
+            blu = 255;
+        if (gre > 255)
+            gre = 255;
+        return (new Color(red, gre, blu));
+    }
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Spawn")
+		{
+			sr.color = additionColor(sr.color, other.gameObject.GetComponent<SpriteRenderer>().color);
+			Destroy(other.gameObject);
+		}
+        if (other.gameObject.tag == "Wall")
+        {
+            other.GetComponent<WallCmp>().updatePlayer(this.gameObject);
+        }
+	}
+}
---
+++ PixelChallenge2018/Assets/script/WallCmp.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCmp : MonoBehaviour {
+
+    public bool re;
+    public bool bl;
+    public bool gr;
+    Color tmp = new Color(0, 0, 0);
+
+    private void Start()
+    {
+        if (re)
+            tmp.r = 255;
+        if (bl)
+            tmp.b = 255;
+        if (gr)
+            tmp.g = 255;
+        GetComponent<SpriteRenderer>().color = tmp;
+        tmp = new Color(0, 0, 0);
+    }
+
+    Color soustractionColor(Color one, Color two)
+    {
+        float red = one.r;
+        float gre = one.g;
+        float blu = one.b;
+        red -= two.r;
+        gre -= two.g;
+        blu -= two.b;
+
+        if (red < 0)
+            red = 0;
+        if (blu < 0)
+            blu = 0;
+        if (gre < 0)
+            gre = 0;
+        return (new Color(red, gre, blu));
+    }
+
+    public void updatePlayer(GameObject player)
+    {
+        if (re)
+            tmp.r = player.gameObject.GetComponent<SpriteRenderer>().color.r;
+        if (bl)
+            tmp.b = player.gameObject.GetComponent<SpriteRenderer>().color.b;
+        if (gr)
+            tmp.g = player.gameObject.GetComponent<SpriteRenderer>().color.g;
+        player.GetComponent<SpriteRenderer>().color = soustractionColor(player.GetComponent<SpriteRenderer>().color, tmp);
+        tmp = new Color(0, 0, 0);
+    }
+}
