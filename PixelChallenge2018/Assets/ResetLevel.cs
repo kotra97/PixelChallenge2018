b3c673655fbf1d01bd7c5aba2ca516b62da28ce0
Nico Lear <learjet45@hotmail.fr>
2018-04-06 15:04:47 -0400

    GameController

---
+++ PixelChallenge2018/Assets/ResetLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Joystick Start"))
+        {
+            Debug.Log("restart");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+	}
+}
